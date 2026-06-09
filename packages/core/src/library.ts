cc1fa1851f6ddf43dbf36c3f5ed70cc22059b01a
wsz987 <792974006@qq.com>
2026-06-09 21:37:25 +0800

    删除废弃链路

--- packages/core/src/library.ts
+++
@@ -1,3 +0,0 @@
-export { startCoreServer } from './api/server.js';
-export { runStage1InstallWorkflow } from './graph/run-stage1-install.js';
-export type { Stage1InstallInput, Stage1InstallResult } from './graph/workflow-contracts.js';
